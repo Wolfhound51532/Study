20b01a860bdba26e3628c33af9e18961ba4bb095
Wolfhound51532 <wolfh0und@mail.ru>
2020-07-18 20:38:05 +0300

    Moved url to vars

--- test_test.py
+++ test_test.py
@@ -3,7 +3,7 @@ import json
 
 
 def c(field_name):
-    raw_result = requests.get(url='https://jsonplaceholder.typicode.com/albums')
+    raw_result = requests.get(url=url+'albums')
     assert raw_result.status_code == 200
     response = json.loads(raw_result.text)
     result = {item[field_name] for item in response}
